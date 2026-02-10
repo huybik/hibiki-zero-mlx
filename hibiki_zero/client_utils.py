4da2b092c8ce98a0c5ecca6b105152a02ab24a6a
Tom Labiausse <tom.labiausse@kyutai.org>
2026-02-10 10:57:36 +0100

    update

--- hibiki_zero/client_utils.py
+++ hibiki_zero/client_utils.py
@@ -2,6 +2,11 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+from pathlib import Path
+import torch
+import sphn
+from IPython.display import Audio, display
+
 
 def get_color_code(color_name: str) -> tuple:
     if color_name == "blue":
@@ -41,3 +46,42 @@ def make_colored_log(
 def log(level: str, msg: str, colored_parts: list[tuple[str, str]] | None = None) -> None:
     """Log something with a given level."""
     print(make_colored_log(level, msg, colored_parts))
+
+
+def audio_read(fpath: Path, to_sample_rate: int | None = None, mono: bool = False) -> tuple[torch.Tensor, int]:
+    """ Read audio fpath and resample at to_sample_rate/transform to mono audio if specified. """
+    wav, sr = sphn.read(fpath)
+    if to_sample_rate is not None and sr != to_sample_rate:
+        wav = sphn.resample(wav, sr, to_sample_rate)
+        sr = to_sample_rate
+    wav_tensor: torch.Tensor = torch.tensor(wav)
+    if wav_tensor.ndim == 1:
+        wav_tensor.unsqueeze(0)
+    elif wav_tensor.ndim == 2:
+        if wav_tensor.shape[0] > 2:
+            raise ValueError(f"Audio {fpath} has too many channels, got {wav_tensor.shape[0]} but expected 1 or 2.")
+        elif wav_tensor.shape[0] == 2 and mono:
+            print(f"Audio {fpath} is stereo, averaging both channels to get a mono audio.")
+            wav_tensor = wav_tensor.mean(dim=0, keepdim=True)            
+    elif wav_tensor.ndim >= 3:
+        raise ValueError(f"Audio {fpath} was loaded into a tensor of unsupported shape {wav_tensor.ndim}")
+    return wav_tensor, sr
+
+
+def stack_and_pad_audio(wavs: list[torch.Tensor], max_len: int | None = None) -> torch.Tensor:
+    """ Stack the given audios on the first dimenion (created), padding them with 0 if needed. """
+    actual_max_len = max(wav.shape[-1] for wav in wavs)
+    if max_len is None:
+        max_len = actual_max_len
+    else:
+        assert max_len >= actual_max_len, (max_len, actual_max_len)
+    other_dims = wavs[0].shape[:-1]
+    out = torch.zeros(len(wavs), *other_dims, max_len, dtype=wavs[0].dtype, device=wavs[0].device)
+    for k, wav in enumerate(wavs):
+        out[k, ..., : wav.shape[-1]] = wav
+    return out
+
+
+def display_audio(wav: torch.Tensor, sample_rate: int):
+    """ Display an audio reader to be used in an interactive notebook. """
+    display(Audio(wav.numpy(), rate=sample_rate))
